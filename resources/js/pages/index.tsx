d3ea483f36a1ee9d9ec6de24c8f7d38b5296dcb0
GuidoMors <guidomors@gmail.com>
2025-04-09 15:25:59 +0200

    changes to index

---
+++ resources/js/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Welcome to the Index Page!</h1>
+      <p>This page is rendered by Inertia when the user is logged in.</p>
+    </div>
+  );
+};
+
+export default Index;
---
+++ run.bat
@@ -0,0 +1,1 @@
+php artisan serve --port=3333 --host=0.0.0.0
